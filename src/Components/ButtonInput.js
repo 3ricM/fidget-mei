0f51b5538f4040082f5073dba1c28a43d370ebfa
emei6793 <emei6793@richmondhillhs.org>
2022-03-12 06:08:47 +0000

    final fidget lab

--- src/Components/ButtonInput.js
+++ src/Components/ButtonInput.js
@@ -1,9 +1,33 @@
 import React, {Component} from 'react';
 
 class ButtonInput extends Component {
-  // Add a constructor with a state
+    constructor(props) {
+        super(props);
+        this.state = {
+          text: null
+        }
+      };
 
-  // Add a render function which returns JSX
+render() {
+    return (
+        <li className="ButtonInput cards-item">
+        <div className="card">
+          <div className="card-content">
+            <div className="card-title">Button</div>
+            <p className="card-text">
+              <div className="input btn-block">
+                <input type="button" value="Click me!" className="btn"/>
+              </div>
+              <div className="output">
+                <label for="buttonOutput">State: </label>
+                <span name="buttonOutput"></span>
+              </div>
+            </p>
+          </div>
+        </div>
+      </li> 
+    )
+}
   
 }
 
